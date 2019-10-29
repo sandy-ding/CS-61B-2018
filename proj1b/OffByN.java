22602f3d5a095954d813a41825e5866f9ef9eb2d
Sandy <dhl15821939286@gmail.com>
2019-10-30 00:14:28 +0800

    debug

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,11 +1,18 @@
 public class OffByN implements CharacterComparator {
-    int n;
+    private int n;
     public OffByN(int N) {
         n = N;
     }
 
     @Override
-    public boolean equalChars(char x, char y) {
+    public boolean equalChars(char X, char Y) {
+        int smallest = 'a';
+        int largest = 'z';
+        int x = Character.toLowerCase(X);
+        int y = Character.toLowerCase(Y);
+        if (x < smallest || x > largest || y < smallest || y > largest) {
+            return false;
+        }
         int diff = x - y;
         return Math.abs(diff) == n;
     }
--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,6 +1,13 @@
 public class OffByOne implements CharacterComparator {
     @Override
-    public boolean equalChars(char x, char y) {
+    public boolean equalChars(char X, char Y) {
+        int smallest = 'a';
+        int largest = 'z';
+        int x = Character.toLowerCase(X);
+        int y = Character.toLowerCase(Y);
+        if (x < smallest || x > largest || y < smallest || y > largest) {
+            return false;
+        }
         int diff = x - y;
         return Math.abs(diff) == 1;
     }
