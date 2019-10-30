a634fccf4df1fb8a2ff47719942a0c2e9a72fff2
Sandy <dhl15821939286@gmail.com>
2019-10-30 20:33:53 +0800

    pass test

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,13 +1,6 @@
 public class OffByOne implements CharacterComparator {
     @Override
     public boolean equalChars(char X, char Y) {
-        int smallest = 'a';
-        int largest = 'z';
-        int x = Character.toLowerCase(X);
-        int y = Character.toLowerCase(Y);
-        if (x < smallest || x > largest || y < smallest || y > largest) {
-            return false;
-        }
         int diff = X - Y;
         return Math.abs(diff) == 1;
     }
