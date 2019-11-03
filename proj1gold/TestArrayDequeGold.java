005283daf49ace12695692112395f44f1bf305c4
sandy-ding <34783996+sandy-ding@users.noreply.github.com>
2019-11-03 21:55:07 +0800

    Update TestArrayDequeGold.java

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -7,32 +7,30 @@ public class TestArrayDequeGold {
 
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> ads1 = new ArrayDequeSolution<>();
+        String message = "";
         boolean test = true;
 
         while (test) {
             double testChoice = StdRandom.uniform();
             if (testChoice < 0.25) {
-                int numberToAdd = (int) StdRandom.uniform(0, 100);
+                Integer numberToAdd = StdRandom.uniform(0, 100);
                 sad1.addFirst(numberToAdd);
                 ads1.addFirst(numberToAdd);
-                assertEquals("Oh noooo!\nThis is bad:\n   Random number " + sad1
-                        + " not equal to " + ads1 + "!", ads1, sad1);
+                message += "addFirst(" + numberToAdd + ")\n";
+                assertEquals(message, ads1.get(0), sad1.get(0));
             } else if (testChoice < 0.5) {
-                int numberToAdd = (int) StdRandom.uniform(0, 100);
+                Integer numberToAdd = StdRandom.uniform(0, 100);
                 sad1.addLast(numberToAdd);
                 ads1.addLast(numberToAdd);
-                assertEquals("Oh noooo!\nThis is bad:\n   Random number " + sad1
-                        + " not equal to " + ads1 + "!", ads1, sad1);
+                message += "addLast(" + numberToAdd + ")\n";
+                assertEquals(message, ads1.getLast(), sad1.get(sad1.size()-1));
             } else if (testChoice < 0.75 && !sad1.isEmpty()) {
-                assertEquals("Oh noooo!\nThis is bad:\n   Random number " + sad1
-                        + " not equal to " + ads1 + "!", ads1.removeFirst(), sad1.removeFirst());
+                message += "removeFirst()\n";
+                assertEquals(message, ads1.removeFirst(), sad1.removeFirst());
             } else if (!sad1.isEmpty()) {
-                assertEquals("Oh noooo!\nThis is bad:\n   Random number " + sad1
-                        + " not equal to " + ads1 + "!", ads1.removeLast(), sad1.removeLast());
+                message += "removeLast()\n";
+                assertEquals(message, ads1.removeLast(), sad1.removeLast());
             }
-        }
-
-        sad1.printDeque();
     }
 
 }
